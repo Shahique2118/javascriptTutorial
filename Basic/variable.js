9b1df1b54f7e07dae3b7ddfe81218cdb490a61a0
Shahique2118 <mohammadshahique2118@gmail.com>
2025-05-05 03:50:53 +0000

    understanding variable

---
+++ Basic/variable.js
@@ -0,0 +1,12 @@
+const userId = 1; //intiger value
+
+let userName = "Adnan"; //string value
+
+let userFollower = 1222;
+
+let userFollowing = 899;
+
+let userLoggedIn = true //boolean value
+
+
+console.table([userId, userName, userFollower, userFollowing, userLoggedIn]);
