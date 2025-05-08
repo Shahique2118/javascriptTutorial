96be9cc0271e4676ab1f88cb04e4af44eb055420
Shahique2118 <mohammadshahique2118@gmail.com>
2025-05-08 03:39:02 +0000

    conversion javascript created

---
+++ Basic/conversion.js
@@ -0,0 +1,12 @@
+let Name = 6;
+
+let stId = "adnan123";
+let newName = String (Name);
+
+let newStId = Number (stId);
+console.log(typeof Name);
+console.log(typeof newName);
+console.log(typeof stId);
+console.log(newStId);
+console.log(typeof newStId);
+
---
+++ Basic/datatype.js
@@ -0,0 +1,13 @@
+// premetive-Datatype
+
+const stId =6 // int
+
+let stname = "Adnan" //string
+
+let stLogedIn = true // boolean
+
+console.log(typeof stId)
+console.log(typeof stLogedIn);
+console.log(typeof stname);
+
+
